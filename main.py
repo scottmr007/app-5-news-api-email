7e5935b881ac46ed3c72ad010717513c5ab0a55f
scottmr007 <mrscott@tutamail.com>
2024-03-03 19:30:57 -0500

    send email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
+from send_email import send_email
 
-api_key = "2ffa5acd3bef4b2c864f5675ac619377"
-url = "https://newsapi.org/v2/everything?q=aaple&from=2024-01-24&" \
-        "sortBy=publishedAt&apiKey=2ffa5acd3bef4b2c864f5675ac619377"
+api_key = "api_key"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-02-04&sortBy=publishedAt&            apiKey=api_key"
 
 r = requests.get(url)
 content = r.json()
 
-for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+body = ""
+for article in content["articles"]:
+    body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+    
+body = body.encode()('utf-8')
+send_email(message=body)
     
     
